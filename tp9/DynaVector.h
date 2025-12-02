be8e10340a076c85f857afb394461b138cffedc2
Salle Noe <nsalle@roucas122.etu.ec-m.fr>
2025-12-02 16:00:36 +0100

    tp9 fin

--- tp9/DynaVector.h
+++ tp9/DynaVector.h
@@ -1,16 +1,42 @@
 #ifndef DYNAV_H
 #define DYNAV_H
+#include <iostream>
+using namespace std;
 
 template <class T> class DynaVector{
 private:
-    int size;
+    int taille;
+    int nb;
     T* valeurs;
-    void agrandirTab();
+    void agrandirTab(){
+        int nouvelleTaille = taille * 2 + 1;
+        T* nouveauTab = new T[nouvelleTaille];
+        for(int i = 0; i < nb; i++) {
+            nouveauTab[i] = valeurs[i];
+        }
+        delete[] valeurs;
+        valeurs = nouveauTab;
+        taille = nouvelleTaille;
+        cout << "Tableau redimensionné à " << taille << " éléments" << endl;
+    }
 public:
-    ~DynaVector();
-    DynaVector(int t=0);
-    void ajouterValeur(T v); 
-}
+    ~DynaVector(){
+        delete[] valeurs;
+    }
+
+    DynaVector(int t=0){
+        taille=t;
+        nb=0;
+        valeurs = new T[taille];
+    }
+    void ajouterValeur(T v){
+        if(nb>=taille-1){
+            agrandirTab();
+        }
+        valeurs[nb] = v;
+        nb++;
+    } 
+};
 
 
 
---
+++ tp9/main.cpp
@@ -0,0 +1,24 @@
+#include "DynaVector.h"
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    float a = 0.5, b = 1.0, c = 2.5;
+    DynaVector<float> vecteur1 = DynaVector(2);
+    vecteur1.ajouterValeur(a);
+    vecteur1.ajouterValeur(b);
+    cout << "vecteur 1 : " << vecteur1.valeurs{
+    if(nb>=taille-1){
+        agrandirTab();
+    }
+    valeurs[nb] = v;
+    nb++;
+}[0] << endl;
+
+    vecteur1.ajouterValeur(c);
+
+
+    return 0;
+}
